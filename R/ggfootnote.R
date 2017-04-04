6a374696fc769786b43d49c28f69f57b09f5db67
Charlie Gibbons <charlie.gibbons@brattle.com>
2017-04-04 14:42:40 -0700

    Added use_small_text arg

--- R/ggfootnote.R
+++ R/ggfootnote.R
@@ -1,10 +1,16 @@
-ggfootnote <- function(plot, ...){
+ggfootnote <- function(plot, ..., use_small_text = TRUE){
   assert_is_identical_to_true(inherits(plot, "ggplot"))
+  assert_is_a_bool(use_small_text)
   dots <- list(...)
   assert_all_are_true(sapply(dots, class) == "character")
-  
+  if(use_small_text){
+      font_size <- 9
+  } else {
+      font_size <- 11
+  }
+
   footnote <- paste(dots, collapse = "\n")
-  g <- arrangeGrob(plot, bottom = textGrob(footnote, x = 0, just = "left", hjust = 0, vjust = 0.5, gp = gpar(fontsize = 9, lineheight = 1)))
+  g <- arrangeGrob(plot, bottom = textGrob(footnote, x = 0, just = "left", hjust = 0, vjust = 0.5, gp = gpar(fontsize = font_size, lineheight = 1)))
   dev.off()
   return(g)
 }
